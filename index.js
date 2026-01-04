ecd6e4e348f00bae1062697c7acec5d0027c3b47
desconocidomarplatense <desconocidomarplatense@gmail.com>
2026-01-04 00:35:36 -0300

    Add async function to wait for OBR connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hoja de personaje lista");
+async function waitForOBR() {
+  while (!window.OBR) {
+    await new Promise(r => setTimeout(r, 100));
+  }
+  return window.OBR;
+}
+
+waitForOBR().then(OBR => {
+  console.log("Conectado a Owlbear");
+
+  OBR.onReady(() => {
+    console.log("Owlbear listo");
+  });
+});
